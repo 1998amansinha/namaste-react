eeab55ddd9dbd268313099e4fbd79f116c58a405
[Aman Sinha] <[1998amansinha@gmail.com]>
2024-09-03 11:59:36 +0530

    React and React-Dom is imported

--- App.js
+++ App.js
@@ -1,26 +1,5 @@
-// Element is created using react
-// const element = React.createElement(
-//   "h1", //type
-//   { id: "create" }, // Attribute {props.id}
-//   "Hello World using React!" // props.children
-// );
-
-// For Nested Structure
-
-/*
-<div id="parent">
-    <div id="child1">
-        <h1 id="print">I am an H1 tag</h1>
-        <h2 id="print">I am an H2 tag</h2> // how can we give this type of multiple nested child . so this can be done using array means creating an array of children
-    </div>
-    <div id="child2">
-        <h1 id="print">I am an H1 tag</h1>
-        <h2 id="print">I am an H2 tag</h2> // how can we give this type of multiple nested child . so this can be done using array means creating an array of children
-    </div>
-</div>
-*/
-
-//so this type of nested structure is created like
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const element = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
@@ -33,12 +12,10 @@ const element = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
-console.log(element); // here React.createElment is just an object with a props
 
 // To render something in the dom in react we use react-dom
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// console.log(root) // here ReactDom.createRoot is {_internalRoot: FiberRootNode} a FiberRootNode.
 
 // now to render the created element on the browser we have to render it using the react-dom.
 root.render(element);
