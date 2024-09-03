f378002fd1bdd59ab6d95d4a8f9830556c9cdce0
[Aman Sinha] <[1998amansinha@gmail.com]>
2024-09-03 20:20:12 +0530

    JSX is introduced

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const element = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "print" }, "I am an H1 tag"), //array of childrens for child
-    React.createElement("h2", { id: "print2" }, "I am an H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-]);
+const heading = React.createElement('h1', {id: 'heading'}, "Namaste React")
 
+const jsxHeading = <h1 id='heading2'>Namaste React using JSX</h1>
 
-// To render something in the dom in react we use react-dom
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-
-// now to render the created element on the browser we have to render it using the react-dom.
-root.render(element);
+root.render(jsxHeading)
