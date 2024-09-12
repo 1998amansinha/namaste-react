e0bd3a36ff3331e306d95ba409f271259fa09aec
[Aman Sinha] <[1998amansinha@gmail.com]>
2024-09-12 12:35:57 +0530

    Login, Logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 
 const Header = () => {
+
+  const [btnName, setBtnName] = useState("Login")
+
+  const handleLoginClick = () => {
+    setBtnName(btnName==="Login" ? "Logout" : "Login");
+  };
+
   return (
     <div className="header">
       <div className="headerLogo">
@@ -15,6 +23,7 @@ const Header = () => {
           <li>About us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button className="login" onClick={handleLoginClick}>{btnName}</button> 
         </ul>
       </div>
     </div>
