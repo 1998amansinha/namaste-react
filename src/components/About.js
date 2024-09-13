550fa991c0741307d2efb4d3e56e207eb635d247
[Aman Sinha] <[1998amansinha@gmail.com]>
2024-09-13 17:55:45 +0530

    Class component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
+import React from "react"
 import User from "./User"
 import UserClass from "./UserClass"
 
-const About = () => {
-    return (
-        <div>
+class About extends React.Component {
+    render() {
+        return (
+            <div>
             <h1>About</h1>
             <h3>This is our about page</h3>
             <div>
@@ -11,7 +13,21 @@ const About = () => {
                 <UserClass name={"Aman Sinha (class)"}/>
             </div>
         </div>
-    )
+        )
+    }
 }
+
+// const About = () => {
+//     return (
+//         <div>
+//             <h1>About</h1>
+//             <h3>This is our about page</h3>
+//             <div>
+//                 <User name={"Aman Sinha (Function)"}/>
+//                 <UserClass name={"Aman Sinha (class)"}/>
+//             </div>
+//         </div>
+//     )
+// }
 
 export default About
